b0ca7be1fe7ddd521e3b1324f71d8d79f6197066
Oleh <olegkaraaa@gmail.com>
2021-10-26 23:23:06 +0300

    finish skill-cards, add slider's base

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,15 @@
 import "vanilla-tilt";
-const leftBlock = document.querySelector(".left-block");
-const photo = document.querySelector(".photo");
-const rightBlock = document.querySelector(".right-block");
+import Splide from "@splidejs/splide";
+import "@splidejs/splide/dist/css/splide.min.css";
 
-leftBlock.addEventListener("click", function (e) {
-  leftBlock.classList.toggle("scroll");
-});
+new Splide(".splide", {
+  type: "loop",
+  padding: "5rem",
+  gap: "1rem",
+  pagination: false,
+  arrows: false,
+}).mount();
+
 VanillaTilt.init(document.querySelectorAll(".skills-item"), {
   max: 25,
   speed: 800,
