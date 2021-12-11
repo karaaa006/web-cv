a8f3cd3ec851117fddb7d9ec23736f28be33f106
Oleh <olegkaraaa@gmail.com>
2021-12-11 21:55:32 +0200

    add projects block, expirience block and university

--- src/js/app.js
+++ src/js/app.js
@@ -4,10 +4,10 @@ import "@splidejs/splide/dist/css/splide.min.css";
 
 new Splide(".splide", {
   type: "loop",
-  padding: "5rem",
+  perPage: 3,
   gap: "1rem",
   pagination: false,
-  arrows: false,
+  arrows: true,
 }).mount();
 
 VanillaTilt.init(document.querySelectorAll(".skills-item"), {
